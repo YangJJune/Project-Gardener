27e43372b3cbf0b8e768838be5c27a4b2896931f
unknown <startoflateness@gmail.com>
2020-05-23 17:39:48 +0900

    구조개편

--- Server/app.js
+++ Server/app.js
@@ -1,34 +1,36 @@
 /*************************************************
- * 2020.05.19
+ * 2020.05.23
  * 
  * program을 총괄하는 기본 파일
- * -------------------note-----------------------
- * session과 res.locals에 대한 공부가 필요함
- * 
- * res.send를 사용하지 않고 res.write를 사용한다.
- * 
- * 한 번의 통신에서 '*'경로가 두 번씩 호출된다.
- * express router에 관한 공부가 필요한듯 싶다.
  *************************************************/
 
+// for Express (middleware)
 const express = require('express');
 const app = express();
-const secret = require('./secret.js');
 
-const HTTPTerminator = require('./routes/HTTPTerminator.js');
-const errHandler = require('./routes/errHandler.js');
+// port number
+const portNum = 3000;
 
-// terminate HTTP communication
+// catch 404
 app.use('*', (req, res, next)=>{
-    HTTPTerminator(req, res, next);
+    // throw 404 error
+    let err = new Error('404 Occurred');
+    err.status = 404;
+    next(err);
 });
 
 // err handler
-app.use('*', (err, req, res, next)=>{
-    errHandler(err, req, res, next);
+app.use((err, req, res, next)=>{
+    // log error stack
+    console.log(err.stack);
+
+    // send error message with http status code
+    res.status(err.status || 500).json({
+        message: err.message
+    });
 });
 
 // listen
-app.listen(secret.port_number, ()=>{
-    console.log(`app listening on port ${secret.port_number}!`);
+app.listen(portNum, ()=>{
+    console.log(`app listening on port ${portNum}!`);
 });
