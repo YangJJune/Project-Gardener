9667ce6d9e01b0dde019cb0974d5571291e04774
unknown <startoflateness@gmail.com>
2020-06-03 21:53:23 +0900

    semi re...ba..s...e.........

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import{ BrowserRouter as Router, Switch, Route} from "react-router-dom";
-
-import Login from './components/Login'
-import Home from './components/Home'
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/" component = {Home} />
-          <Route exact path="/login" component = {Login} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
---
+++ Client/test_별찬/PostCardList.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PostCard from './PostCard';
+
+// classify the number of postCards, horizontally, into common display resolutions depending on screen display width.
+const getColumnCount = (width) => {
+  const xWide = 1920;
+  const wide = 1600;
+  const xLarge = 1280;
+  const large = 1024;
+
+  if (!width) return 1;
+  if (width < large) return 1;
+  if (width < xLarge) return 2;
+  if (width < wide) return 3;
+  if (width < xWide) return 4;
+  return 5;
+};
+
+const PostCardList = ({ posts, width }) => {
+  // this scope has access to jsx
+  const columnCount = getColumnCount(width);
+  const postList = posts
+    .slice(0, posts.length)
+    .map((post) => (
+      <PostCard
+        title={post.title}
+        summary={post.summary}
+        date={post.date}
+        username={post.username}
+      />
+    ));
+
+  return (
+    <>
+      <div className="PostCardList">
+        {
+          // check from left to right if operand is falsy
+          // if operand is falsy, return the value of it
+          // otherwise, if all of the operands are considered true, return div element.
+          // boolean false and empty array is not rendered.
+          postList && postList.length === 0 && (
+            <div className="empty-list">아직 작성한 포스트가 없습니다.</div>
+          )
+        }
+      </div>
+    </>
+  );
+};
+
+PostCardList.defaultProps = {
+  posts: [],
+  width: 0,
+};
