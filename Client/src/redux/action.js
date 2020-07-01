35120114ddd4193c647a0e7fe85f2ff7a6ec7d26
unknown <startoflateness@gmail.com>
2020-07-01 17:31:34 +0900

    redux 기초 디자인

--- Client/src/redux/action.js
+++ Client/src/redux/action.js
@@ -1,30 +1,111 @@
+/********************************************
+ * redux action과 action creator를 정의하는 파일
+ * --------------------------------------------
+ * fetch "gethub access token"
+ * fetch "user info"
+ * fetch "article list"
+ * 
+ * ## 중요 ##
+ * axios 입력을 작성하지 않음
+ ********************************************/
+
 import axios from 'axios';
 
-export const REQUEST_HTTP = 'REQUEST_HTTP';
-export const RESPONSE_HTTP = 'RESPONSE_HTTP';
+// the action associated with getting "GitHub access token"
+export const REQUEST_GH_TOKEN = 'REQUEST_GH_TOKEN';
+export const RECEIVE_GH_TOKEN = 'RECEIVE_GH_TOKEN';
+export const REQUEST_USER_INFO = 'REQUEST_USER_INFO';
+export const RECEIVE_USER_INFO = 'RECEIVE_USER_INFO';
 
-export const requestHttp = (request) => () => ({
-  type: REQUEST_HTTP,
-  payload: request,
-});
-
-export const responseHttp = (response) => () => ({
-  type: RESPONSE_HTTP,
-  payload: response,
-});
-
-const getResponse = async (request, dispatch) => {
-  const response = await axios(request);
-  return dispatch(responseHttp(response));
-};
-
-export const fetchHttp = (request) => (dispatch) => {
-  dispatch(requestHttp(request));
-  return getResponse(request, dispatch);
-};
-
-export const fetchIfNotFetching = (request) => (dispatch, getState) => {
-  if (getState().isFetching === false) {
-    return dispatch(fetchHttp(request));
+const requestGHToken = () =>{
+  return {
+    type: REQUEST_GH_TOKEN
   }
-};
+}
+const receiveGHToken = (token) =>{
+  return {
+    type: RECEIVE_GH_TOKEN,
+    payload: {
+      accessToken : token
+    }
+  }
+}
+const requestUserInfo = () => {
+  return {
+    type: REQUEST_USER_INFO
+  }
+}
+const receiveUserInfo = (userName) => {
+  return {
+    type: RECEIVE_USER_INFO,
+    payload: {
+      userName: userName
+    }
+  }
+}
+
+const fetchGHToken = async (code) => {
+  return (dispatch) => {
+    dispatch(requestGHToken())
+    const token = await axios()
+    dispatch(receiveGHToken(token))
+  }
+}
+
+const fetchUserInfo = async () => {
+  return (dispatch) => {
+    dispatch(requestUserInfo())
+    const userInfo = await axios()
+    dispatch(receiveUserInfo(userInfo.login))
+  }
+}
+
+export const fetchGHTokenIfNotFetching = (code) =>{
+  return (dispatch, getState) => {
+    if(getState().userInfo.isFetching === false){
+      return dispatch(fetchGHToken(code))
+    }
+  }
+}
+
+export const fetchUserInfoIfNotFetching = () => {
+  return (dispatch, getState) => {
+    if(getState().userInfo.isFetching === false){
+      return dispatch(fetchUserInfo())
+    }
+  }
+}
+
+// the action associated with getting "article list"
+export const REQUEST_ARTICLE_LIST = 'REQUEST_ARTICLE_LIST'
+export const RECEIVE_ARTICLE_LIST = 'RECEIVE_ARTICLE_LIST'
+
+const requestArticleList = () => {
+  return {
+    type: REQUEST_ARTICLE_LIST
+  }
+}
+const receiveArticleList = (list) => {
+  return {
+    type: RECEIVE_ARTICLE_LIST,
+    payload: {
+      articleList : list
+    }
+  }
+}
+
+const fetchArticleList = async (filter) => {
+  return (dispatch) => {
+    dispatch(requestArticleList())
+    const list = await axios()
+    dispatch(receiveArticleList(list))
+  }
+}
+
+export const fetchArticleListIfNotFetching = (filter) => {
+  return (dispatch, getState) => {
+    if(getState().articleList.isFetching === false){
+      return dispatch(fetchArticleList(filter))
+    }
+  }
+}
--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,48 +1,70 @@
-const LOGIN = 'LOGIN';
-const LOGOUT = 'LOGOUT';
+/******************************************
+ * redux reducer를 정의하는 파일
+ * ----------------------------------------
+ * ## 주의 ##
+ * 현재 작성된 reducer는 store를 2개로 
+ * 분리했음을 가정하고 작성됨
+ ******************************************/
 
-const appInfo = {
-  appName: 'PRJ::GRDNER',
-};
-// handles static data related to app info
-const appNameReducer = (state = appInfo, action) => {
-  switch (action.type) {
-    default:
-      return state;
-  }
-};
+// the reducer associated with handling login and logout process
+import {REQUEST_GH_TOKEN, RECEIVE_GH_TOKEN, REQUEST_USER_INFO, RECEIVE_USER_INFO} from './action'
 
-const defaultLoginState = {
+const defaultUserState = {
   isLoggedIn: false,
-  userName: null,
+  isFetching: false,
+  userName: 'guest',
+  accessToken : 'ghtoken',
 };
-// handles login and logout process
-const loginReducer = (state = defaultLoginState, action) => {
+export const loginReducer = (state = defaultUserState, action) => {
   switch (action.type) {
-    case LOGIN:
-      localStorage.setItem(
-        'isLoggedIn',
-        JSON.stringify(action.payload.isLoggedIn)
-      );
-      localStorage.setItem('username', JSON.stringify(action.payload.username));
-      console.log(action.payload.isLoggedIn);
-      return {
-        ...state,
-        isLoggedIn: action.payload.isLoggedIn,
-        userName: action.payload.userName,
-      };
-    case LOGOUT:
-      localStorage.clear();
-      return {
-        ...state,
-        isLoggedIn: false,
-        userName: null,
-      };
+    case REQUEST_GH_TOKEN:
+      return Object.assign({}, state, {
+        isFetching: true,
+      })
+    case RECEIVE_GH_TOKEN:
+      return Object.assign({}, state, {
+        isFetching: false,
+        accessToken : action.payload.accessToken,
+      })
+    case REQUEST_USER_INFO:
+      return Object.assign({}, state, {
+        isFetching: true,
+      })
+    case RECEIVE_USER_INFO:
+    return Object.assign({}, state, {
+      isLoggedIn: true,
+      isFetching: false,
+      userName: action.payload.userName,
+    })
     default:
       return state;
   }
 };
 
+// the reducer associated with handling article list
+import {REQUEST_ARTICLE_LIST, RECEIVE_ARTICLE_LIST} from './action'
+
+const defaultArticleListState = {
+  isFetching: false,
+  articleList: [],
+};
+export const articleListReducer = (state = defaultUserState, action) => {
+  switch (action.type) {
+    case REQUEST_ARTICLE_LIST:
+      return Object.assign({}, state, {
+        isFetching: true,
+      })
+    case RECEIVE_ARTICLE_LIST:
+      return Object.assign({}, state, {
+        isFetching: false,
+        articleList: action.payload.articleList
+      })
+    default:
+      return state;
+  }
+};
+
+/*
 const defaultPostState = null;
 // 게시물과 관련된 reducer
 const postReducer = (state = defaultPostState, action) => {
@@ -68,3 +90,40 @@ const userInfoReducer = (state = defaultUserName, action) => {
       return state;
   }
 };
+const loginReducer = (state = defaultUserState, action) => {
+  switch (action.type) {
+    case LOGIN:
+      localStorage.setItem(
+        'isLoggedIn',
+        JSON.stringify(action.payload.isLoggedIn)
+      );
+      localStorage.setItem('username', JSON.stringify(action.payload.username));
+      console.log(action.payload.isLoggedIn);
+      return {
+        ...state,
+        isLoggedIn: action.payload.isLoggedIn,
+        userName: action.payload.userName,
+      };
+    case LOGOUT:
+      localStorage.clear();
+      return {
+        ...state,
+        isLoggedIn: false,
+        userName: null,
+      };
+    default:
+      return state;
+  }
+};
+
+const appInfo = {
+  appName: 'PRJ::GRDNER',
+};
+// handles static data related to app info
+const appNameReducer = (state = appInfo, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+*/
