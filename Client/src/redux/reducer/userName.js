3d6269952796642e65818b1467d7b261ed54ef37
algoORgoal <qufstar46@gmail.com>
2020-07-04 00:23:33 +0900

    simplify functon expression

--- Client/src/redux/reducer/userName.js
+++ Client/src/redux/reducer/userName.js
@@ -2,26 +2,20 @@
  * Git-hub user name과 관계된 reducer를 정의
  ******************************************/
 
-import {REQUEST_USER_NAME, RECEIVE_USER_NAME } from '../action/userNameAction'
+import { REQUEST_USER_NAME, RECEIVE_USER_NAME } from '../action/userNameAction';
 
 const defaultState = {
-    isFetching: false,
-    userName: null,
-}
+  isFetching: false,
+  userName: null,
+};
 
-export const userName = 
-    function userNameReducer(state = defaultState, action){
-        switch (action.type) {
-            case REQUEST_USER_NAME:
-                return { ...state,
-                    isFetching: true,
-                }
-            case RECEIVE_USER_NAME:
-                return { ...state,
-                    isFetching: false,
-                    userName: action.payload.userName,
-                }
-            default:
-                return state
-        }
-    }
+export const userName = (state = defaultState, action) => {
+  switch (action.type) {
+    case REQUEST_USER_NAME:
+      return { ...state, isFetching: true };
+    case RECEIVE_USER_NAME:
+      return { ...state, isFetching: false, userName: action.payload.userName };
+    default:
+      return state;
+  }
+};
