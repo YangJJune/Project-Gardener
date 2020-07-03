1e186c04e5dc7c0b2d4693797bd4a4cdec5e3688
algoORgoal <qufstar46@gmail.com>
2020-07-04 01:50:25 +0900

    format code

--- Client/src/redux/action/GHTokenAction.js
+++ Client/src/redux/action/GHTokenAction.js
@@ -1,5 +1,5 @@
 /********************************************
- * Git-hub access token과 관계된 
+ * Git-hub access token과 관계된
  * action, action creator, fetcher를 정의
  *
  * redux-state의 userInfo field와 연관
@@ -8,40 +8,40 @@
  * axios 입력을 작성하지 않음
  ********************************************/
 
-import axios from 'axios'
+import axios from 'axios';
 
 // define action
-export const REQUEST_GH_TOKEN = 'REQUEST_GH_TOKEN'
-export const RECEIVE_GH_TOKEN = 'RECEIVE_GH_TOKEN'
+export const REQUEST_GH_TOKEN = 'REQUEST_GH_TOKEN';
+export const RECEIVE_GH_TOKEN = 'RECEIVE_GH_TOKEN';
 
 // define action creator
 const requestGHToken = () => {
   return {
     type: REQUEST_GH_TOKEN,
-  }
-}
+  };
+};
 const receiveGHToken = (token) => {
   return {
     type: RECEIVE_GH_TOKEN,
     payload: {
       accessToken: token,
     },
-  }
-}
+  };
+};
 
 // define fetcher
 const fetchGHToken = (code, requestGenerator) => {
   return async (dispatch) => {
-    dispatch(requestGHToken())
-    const token = (await axios(requestGenerator(code))).data.access_token
-    dispatch(receiveGHToken(token))
-  }
-}
+    dispatch(requestGHToken());
+    const token = (await axios(requestGenerator(code))).data.access_token;
+    dispatch(receiveGHToken(token));
+  };
+};
 
 export const fetchGHTokenIfNotFetching = (code, requestGenerator) => {
   return (dispatch, getState) => {
     if (getState().GHToken.isFetching === false) {
-      return dispatch(fetchGHToken(code, requestGenerator))
+      return dispatch(fetchGHToken(code, requestGenerator));
     }
-  }
-}
+  };
+};
