62e48e9ba6d1ef1896a67fdfc61bc1d87aa94ab9
unknown <startoflateness@gmail.com>
2020-07-04 08:44:52 +0900

    delay redirection

--- Client/src/components/redirection/LoginGH.js
+++ Client/src/components/redirection/LoginGH.js
@@ -1,6 +1,11 @@
 /*******************************************
  * url parameter의 code를 이용해서
  * redux-state의 userInfo.accessToken을 설정한다.
+ * -----------------------------------------
+ * note
+ * for delayed redirecion, use history not Redirect
+ * 만약 Redirect 컴포넌트로 delayed redirection이 
+ * 가능한 방법을 찾는다면 알려주길 바람.
  *******************************************/
 
 import React, { useEffect } from 'react';
@@ -9,16 +14,21 @@ import { Redirect } from 'react-router-dom';
 import qs from 'qs';
 import { loginGH } from '../../helpers/requestToGHHelper';
 
-export default function LoginGH({ location }) {
+export default function LoginGH({ location, history }) {
   const dispatch = useDispatch();
+  const loginAndRedirect = async function loginAndRedirect(){
+    const code = qs.parse(location.search.slice(1)).code;
+    await dispatch(loginGH(code));
+    history.replace('/redirection/checkGarden')
+  }
 
   useEffect(() => {
-    const code = qs.parse(location.search.slice(1)).code;
-    dispatch(loginGH(code));
+    loginAndRedirect()
   });
 
   return (
     // redirect to get user information page
-    <Redirect to='/' />
+    //<Redirect to='/' />
+    <h3>loading...</h3>
   );
 }
