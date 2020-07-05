85d2cf534e8d708866983957e6e77f54a2ab9a83
pjy1368@konkuk.ac.kr <pjy1368@konkuk.ac.kr>
2020-07-05 09:41:04 +0900

    update

--- Client/src/components/home/Home.js
+++ Client/src/components/home/Home.js
@@ -4,35 +4,15 @@
  * 글을 보여주거나, 글을 작성한다.
  *******************************************/
 
-<<<<<<< HEAD
-import React from 'react'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Header from './header/Header'
-import Writer from './writer/Writer'
-import Viewer from './viewer/Viewer'
-import PostCardList from './postCardList/PostCardList'
-=======
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './header/Header';
 import Writer from './writer/Writer';
 import Viewer from './viewer/Viewer';
 import PostCardList from './postCardList/PostCardList';
->>>>>>> f6c30cadf9dfe6139abb019343d270af5c135660
 
 function Home({ match }) {
   return (
-<<<<<<< HEAD
-    <BrowserRouter basename={match.url}>
-        <Header />
-        <Switch>
-            <Route path='/write' component={Writer} />
-            <Route path='/view' component={Viewer} />
-            <Route path='/' component={PostCardList} />
-        </Switch>
-    </BrowserRouter>
-  )
-=======
     <section>
       <Header />
       <Switch>
@@ -42,7 +22,6 @@ function Home({ match }) {
       </Switch>
     </section>
   );
->>>>>>> f6c30cadf9dfe6139abb019343d270af5c135660
 }
 
 export default Home;
