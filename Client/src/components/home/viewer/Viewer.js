03e824547ea760300fb11e741ddb440c38a2db66
unknown <startoflateness@gmail.com>
2020-07-05 18:00:40 +0900

    viewer 제작 (test 불가능)

--- Client/src/components/home/viewer/Viewer.js
+++ Client/src/components/home/viewer/Viewer.js
@@ -1,18 +1,56 @@
 /****************************
  * Article contents를 보여주는 component
- * ------------------------------------
+ * ----------------------------
  * XXX
- * 구현 안 됨
+ * redux-state 미구현으로 인한 test 불가능
  ****************************/
 
 import React from 'react'
+import {Link} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
-function Viewer() {
+export default function Viewer() {
+  const {article, isFetching} = useSelector((state) => state.selectedArticle)
+  const userName = useSelector((state) => state.userName.userName)
+  
+  const deleteArticleBtnGenerator = 
+    function deleteArticleBtnGenerator(){
+      if(userName === article.author){
+        return (
+          <Link to='/redirection/deleteArticle'>
+            delete article
+          </Link>
+        )
+      }
+    }
+
+  const viewGenerator =
+    function viewGenerator(){
+      if(isFetching)
+        return (
+          <h3>loading...</h3>
+        )
+
+      return (
+        <div>
+          <h2>{article.title}</h2>
+          <div>
+            {article.author}
+          </div>
+          <div>
+            {article.category}
+          </div>
+          <div>
+            {article.topic}
+          </div>
+          <div>
+            {article.content}
+          </div>
+          {deleteArticleBtnGenerator()}
+        </div>)
+    }
+
   return (
-    <div>
-      
-    </div>
+    viewGenerator()
   )
 }
-
-export default Viewer
